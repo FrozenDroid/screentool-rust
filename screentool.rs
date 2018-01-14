e7fda7dedfbdad36d66d52b2712cc624032b54ea
Vincent Stakenburg <vincentstakenburg@gmail.com>
2018-01-15 00:11:29 +0100

    got the option buttons to work

--- screentool.rs
+++ screentool.rs
@@ -3,7 +3,7 @@ extern crate gtk;
 extern crate gdk;
 
 use gtk::prelude::*;
-use gtk::{Window, WindowType, Dialog, DialogFlags, Label, ListBox, ListBoxRow, CheckButton};
+use gtk::{Window, WindowType, Dialog, DialogFlags, Label, ListBox, ListBoxRow, CheckButton, RadioButton};
 
 fn main() {
     if gtk::init().is_err() {
@@ -20,48 +20,54 @@ fn main() {
         &[(&"Hello", 1)]
     );
 
-    //dialog.get_style_context()
-    //    .unwrap()
-    //    .set_property("use-header-bar", &(1 as i32))
-    //    .expect("Error while setting spacing");
-
-    // dialog.set_property_margin(10);
-
-    
     let content_area = Dialog::get_content_area(&dialog);
 
     content_area.set_property_margin(10);
 
     let label = Label::new(Some("Test"));
-    let label2 = Label::new(Some("Booooop"));
 
     let list_box = ListBox::new();
-    let list_box_row = ListBoxRow::new();
-
-    let color = gdk::RGBA { alpha: 100.0, blue: 255.0, green: 0.0, red: 0.0};
-
-    let check_button = CheckButton::new_with_label("boooop");
-    println!("{:?}", check_button.get_child().unwrap().downcast::<gtk::Label>().unwrap().set_markup("<span foreground=\"black\">boop!</span>"));
-    //check_button.override_color(gtk::StateFlags::NORMAL, &color);
-    list_box_row.add(&check_button);
-    addOptionRow("Boop", &list_box);
-    list_box_row.add(&check_button.clone());
-    list_box_row.add(&check_button.clone());
-    list_box.add(&list_box_row);
+    add_option_row("Boop", &list_box);
+    add_option_row("Boop1", &list_box);
+    add_option_row("Boop2", &list_box);
 
     content_area.add(&label);
     content_area.add(&list_box);
-    // window.set_title("Test");
-    // window.set_default_size(350, 70);
-
-    //window.add(&dialog);
-
     dialog.show_all();
 
     gtk::main();
 }
 
-fn addOptionRow(s: &str, row: &ListBox) {
+fn add_option_row(s: &str, list_box: &ListBox) {
+    let list_box_row = ListBoxRow::new();
+    let children = list_box.get_children();
+    let radio_button: RadioButton;
 
+    let last_child = children.last();
+    if let Some(last_child) = last_child {
+        radio_button = RadioButton::new_with_label_from_widget(
+            &last_child.clone()
+                .downcast::<ListBoxRow>()
+                .unwrap()
+                .get_children()
+                .first()
+                .unwrap()
+                .clone()
+                .downcast::<RadioButton>()
+                .unwrap(), 
+            ""
+        );
+    } else {
+        radio_button = RadioButton::new_with_label("");
+    }
+
+    radio_button.get_child()
+        .unwrap()
+        .downcast::<gtk::Label>()
+        .unwrap()
+        .set_markup(format!("<span foreground=\"black\">{}</span>", s).as_str());
+
+    list_box_row.add(&radio_button);
+    list_box.add(&list_box_row);
 }
 
