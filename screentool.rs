8a24311aa6cbed721acd066a357840c5d6c6f7c7
Vincent Stakenburg <vincentstakenburg@gmail.com>
2018-01-22 23:02:36 +0100

    Fixed issue where it does not fade the window.

--- screentool.rs
+++ screentool.rs
@@ -1,13 +1,15 @@
-extern crate gtk;
-extern crate gdk;
 extern crate clap;
+extern crate gdk;
+extern crate gtk;
 
 use gtk::prelude::*;
-use gtk::{Dialog, Builder, ComboBoxText, Button};
+use gtk::{Builder, Button, ComboBoxText, Dialog};
 use clap::{App, Arg};
 use std::process::exit;
 use std::rc::Rc;
 use std::cell::Cell;
+use std::process::Command;
+use std::thread::sleep;
 
 macro_rules! clone {
     (@param _) => ( _ );
@@ -30,34 +32,41 @@ macro_rules! clone {
 enum Action {
     Screenshot,
     Record,
-    Cancel
+    Cancel,
 }
 
 fn action_from_string(string: String) -> Action {
     match string.to_lowercase().as_str() {
         "screenshot" => Action::Screenshot,
         "record" | "recording" => Action::Record,
-        _ => Action::Cancel
+        _ => Action::Cancel,
     }
 }
 
 fn main() {
-    let matches = App::new("screentool-rust").version("0.1")
-                    .arg(Arg::with_name("action")
-                        .short("a")
-                        .long("action")
-                        .help("Takes in either 'screenshot' or 'record'")
-                        .takes_value(true))
-                    .get_matches();
+    let matches = App::new("screentool-rust")
+        .version("0.1")
+        .arg(
+            Arg::with_name("action")
+                .short("a")
+                .long("action")
+                .help("Takes in either 'screenshot' or 'record'")
+                .takes_value(true),
+        )
+        .get_matches();
 
-    let action = matches.value_of("action").map(|s| action_from_string(s.to_string())).unwrap_or_else(type_dialog);
+    let action = matches
+        .value_of("action")
+        .map(|s| action_from_string(s.to_string()))
+        .unwrap_or_else(type_dialog);
 
     match action {
         Action::Screenshot => take_screenshot(),
         Action::Record => record_screen(),
-        _ => exit(0) 
+        _ => exit(0),
     }
 
+    while (true) {}
 }
 
 fn type_dialog() -> Action {
@@ -69,27 +78,37 @@ fn type_dialog() -> Action {
     let type_dialog_glade = include_str!("type-dialog.glade");
     let builder = Builder::new_from_string(type_dialog_glade);
 
-    let dialog: Dialog = builder.get_object("type_dialog").expect("Couldn't get type_dialog");
-    let type_list: ComboBoxText = builder.get_object("type_combobox").expect("Couldn't get type_list");
-    let continue_button: Button = builder.get_object("continue_button").expect("Couldn't get continue_button");
-    let cancel_button: Button = builder.get_object("cancel_button").expect("Couldn't get cancel_button");
+    let dialog: Dialog = builder
+        .get_object("type_dialog")
+        .expect("Couldn't get type_dialog");
+    let type_list: ComboBoxText = builder
+        .get_object("type_combobox")
+        .expect("Couldn't get type_list");
+    let continue_button: Button = builder
+        .get_object("continue_button")
+        .expect("Couldn't get continue_button");
+    let cancel_button: Button = builder
+        .get_object("cancel_button")
+        .expect("Couldn't get cancel_button");
 
     let action = Rc::new(Cell::new(Action::Cancel));
 
-    dialog.connect_delete_event(move |_,_| {
+    dialog.connect_delete_event(clone!(dialog => move |_, _| {
+        dialog.destroy();
         gtk::main_quit();
         Inhibit(false)
-    });
+    }));
 
-    cancel_button.connect_clicked(move |_| {
+    cancel_button.connect_clicked(clone!(dialog => move |_| {
+        dialog.destroy();
         gtk::main_quit();
-    });
+    }));
 
-    continue_button.connect_clicked(clone!(action, type_list => move |_| {
-        println!("{:?}", type_list.get_active_text());
+    continue_button.connect_clicked(clone!(action, type_list, dialog => move |_| {
         if let Some(active_text) = type_list.get_active_text() {
             action.set(action_from_string(active_text));
         }
+        dialog.destroy();
         gtk::main_quit();
     }));
 
@@ -105,5 +124,11 @@ fn record_screen() {
 
 fn take_screenshot() {
     println!("Take screenshot!");
+    println!(
+        "{:?}",
+        Command::new("maim")
+            .arg("-s")
+            .output()
+            .expect("Failed to take screenshot")
+    );
 }
-
